26c6a95b9c5efde8f70af2b33a640c55529432b9
mhshakil09 <mhshakil09@gmail.com>
2020-10-28 02:28:57 +0600

    Weather update using OpenWeather API

--- app/src/main/java/com/example/weathertesting/MainActivity.java
+++ app/src/main/java/com/example/weathertesting/MainActivity.java
@@ -10,10 +10,13 @@ import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -45,13 +48,16 @@ public class MainActivity extends AppCompatActivity {
     public String CITY_NAME = "";
     public String API_URL = "&appid=d98a770b1eb43daa53a864e19c9e19ed&units=metric";
 
-    Button btnSearch;
-    TextView tvDate, tvTemp, tvDescription, tvOutput;
+    ImageButton btnSearch;
+    TextView tvDate, tvTemp, tvDegreeC, tvDescription, tvOutput,tvMax, tvMin, tvFeelsLike;
+    ImageView ivDescription;
     EditText etCity;
     private RequestQueue queue;
 
     //for getting the current location
     FusedLocationProviderClient fusedLocationProviderClient;
+
+    private static long back_pressed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,10 +66,15 @@ public class MainActivity extends AppCompatActivity {
 
         tvDate = findViewById(R.id.tvDate);
         tvTemp = findViewById(R.id.tvTemp);
+        tvDegreeC = findViewById(R.id.tvDegreeC);
         tvDescription = findViewById(R.id.tvDescription);
         tvOutput = findViewById(R.id.tvOutput);
+        tvMin = findViewById(R.id.tvMin);
+        tvMax = findViewById(R.id.tvMax);
+        tvFeelsLike = findViewById(R.id.tvFeelsLike);
         etCity = findViewById(R.id.etCity);
         btnSearch = findViewById(R.id.btnSearch);
+        ivDescription = findViewById(R.id.ivDescription);
 
 
 //        Toast toast = Toast.makeText(getApplicationContext(), "hola!", Toast.LENGTH_SHORT);
@@ -72,7 +83,7 @@ public class MainActivity extends AppCompatActivity {
         //for getting the current location
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
         if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            btnSearch.setText("search");
+//            btnSearch.setText("search");
             getLocation();
         } else {
             ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
@@ -89,7 +100,7 @@ public class MainActivity extends AppCompatActivity {
                     findWeather();
                 }else {
                     if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                        btnSearch.setText("search");
+//                        btnSearch.setText("search");
                         getLocation();
                     } else {
                         ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
@@ -106,7 +117,20 @@ public class MainActivity extends AppCompatActivity {
 //        findWeather();
 
 
+
     }
+
+
+    // Double press back to Exit
+    @Override
+    public void onBackPressed()
+    {
+        if (back_pressed + 2000 > System.currentTimeMillis()) super.onBackPressed();
+        else Toast.makeText(getBaseContext(), "Press once again to exit!", Toast.LENGTH_SHORT).show();
+        back_pressed = System.currentTimeMillis();
+    }
+
+
 
     private void getLocation() {
 
@@ -136,6 +160,8 @@ public class MainActivity extends AppCompatActivity {
 //                        etCity.setText(addresses.get(0).getLocality());
                         CITY_NAME = addresses.get(0).getLocality();
                         etCity.setText(CITY_NAME);
+                        Toast toast = Toast.makeText(getApplicationContext(), "Current City!", Toast.LENGTH_SHORT);
+                        toast.show();
                         findWeather();
                         CITY_NAME = "";
 
@@ -173,8 +199,6 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-
-
 //--------------------------------------------------------------------------------------------------
     /*
     try {
@@ -204,18 +228,6 @@ public class MainActivity extends AppCompatActivity {
 //--------------------------------------------------------------------------------------------------
 
 
-
-
-
-
-
-
-
-
-
-
-
-
     public void findWeather(){
         String url = BASE_URL+CITY_NAME+API_URL;
 //        String url = "http://api.openweathermap.org/data/2.5/weather?q=dhaka&appid=d98a770b1eb43daa53a864e19c9e19ed&units=metric";
@@ -225,14 +237,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onResponse(JSONObject response) {
                 Log.e("apiResponse", response.toString());
-                Toast toast = Toast.makeText(getApplicationContext(), "success!", Toast.LENGTH_SHORT);
-                toast.show();
+//                Toast toast = Toast.makeText(getApplicationContext(), "success!", Toast.LENGTH_SHORT);
+//                toast.show();
                 try {
                     int cod = response.getInt("cod");
 
                     if (cod == 200){
-                        toast = Toast.makeText(getApplicationContext(), "city found!", Toast.LENGTH_SHORT);
-                        toast.show();
+//                        toast = Toast.makeText(getApplicationContext(), "city found!", Toast.LENGTH_SHORT);
+//                        toast.show();
 
 
                         // getting and setting the temperature
@@ -240,22 +252,44 @@ public class MainActivity extends AppCompatActivity {
                         Log.e("mainObject", main_object.toString());
                         String tempObj = main_object.getString("temp");
                         Log.e("tempObj", tempObj);
-                        String temp = String.valueOf(main_object.getDouble("temp"));
+                        String temp = String.valueOf(main_object.getInt("temp"));
 
                         // setting the condition
                         JSONArray array = response.getJSONArray("weather");
                         JSONObject object = array.getJSONObject(0);
                         String description = object.getString("description");
+                        String icon = object.getString("icon");
 
                         // City name
                         String city = response.getString("name");
 //                        etCity.setText(CITY_NAME);
 
                         // Setting temperature
-                        tvTemp.setText("Temperature: " + temp+ "\u2103");
+                        tvTemp.setText(temp);
+                        tvDegreeC.setText("\u2103");
 
-                        // setting weather condition
-                        tvDescription.setText("Condition: "+description);
+                        // setting weather condition nad icon
+                        if (icon.equals("50n")|| icon.equals("50d")){
+//                            ivDescription.setImageResource(@drawable/);
+                            ivDescription.setBackgroundResource(R.drawable.haze_mist);
+                        }else if (icon.equals("02n")|| icon.equals("02d")){
+                            ivDescription.setBackgroundResource(R.drawable.few_clouds);
+                        }else if (icon.equals("03n")|| icon.equals("03d")){
+                            ivDescription.setBackgroundResource(R.drawable.scatteredclouds);
+                        }else if (icon.equals("04n")|| icon.equals("04d")){
+                            ivDescription.setBackgroundResource(R.drawable.broken_clouds);
+                        }else if (icon.equals("09n")|| icon.equals("09d")){
+                            ivDescription.setBackgroundResource(R.drawable.shower_rain);
+                        }else if (icon.equals("10n")|| icon.equals("10d")){
+                            ivDescription.setBackgroundResource(R.drawable.rain);
+                        }else if (icon.equals("11n")|| icon.equals("11d")){
+                            ivDescription.setBackgroundResource(R.drawable.thunderstorm);
+                        }else if (icon.equals("13n")|| icon.equals("13d")){
+                            ivDescription.setBackgroundResource(R.drawable.snow);
+                        }else {
+                            ivDescription.setBackgroundResource(R.drawable.clear_sky);
+                        }
+                        tvDescription.setText(description);
 
                         //setting and formatting date
                         Calendar calendar = Calendar.getInstance();
@@ -278,7 +312,8 @@ public class MainActivity extends AppCompatActivity {
                         String mainHumidity = main_object.getString("humidity");
 
                         //visibility
-                        int visibility = response.getInt("visibility");
+                        double visibility = response.getDouble("visibility");
+                        visibility = visibility/1000.0;
 //                    String visibility = response.getString("visibility");
 
                         // for wind
@@ -307,34 +342,40 @@ public class MainActivity extends AppCompatActivity {
                         String extraName = response.getString("name");
                         String extraCod = response.getString("cod");
 
+                        // minimum and maximum temperature
+                        tvMin.setText(mainMin + "\u00b0\u2193");
+                        tvMax.setText(mainMax + "\u00b0\u2191");
+
+                        // feels like
+                        tvFeelsLike.setText("Feels like " + mainFeelsLike + "\u00b0");
 
 
-
-                        tvOutput.setText("Co-ordinate: "+tempLat+" by "+tempLon+"\n"+
-                                "Feels like: "+mainFeelsLike+"\u2103\n"+
-                                "Min temp: "+mainMin+"\u2103\n"+
-                                "Max temp: "+mainMax+"\u2103\n"+
-                                "Pressure: "+mainPressure+"\n"+
-                                "Humidity: "+mainHumidity+"\n"+
-                                "Visibility: "+visibility+"\n"+
-                                "Wind Speed: "+windSpeed+"\n"+
-                                "Wind Degree: "+windDegree+"\n"+
-                                "DT: "+dt+"\n"+
-                                "Type: "+sysType+"\n"+
-                                "Id: "+sysId+"\n"+
-                                "Country: "+sysCountry+"\n"+
-                                "Sunrise: "+sysSunrise+"\n"+
-                                "Sunset: "+sysSunset+"\n"+
-                                "TimeZone: "+extraTimeZone+"\n"+
-                                "Id: "+extraId+"\n"+
-                                "Name: "+extraName+"\n"+
-                                "Cod: "+extraCod+"\n"+
+                        tvOutput.setText(
+//                                "Co-ordinate: "+tempLat+" by "+tempLon+"\n"+
+//                                "Feels like: "+mainFeelsLike+"\u2103\n"+
+//                                "Min temp: "+mainMin+"\u2103\n"+
+//                                "Max temp: "+mainMax+"\u2103\n"+
+//                                "Pressure: "+mainPressure+"\n"+
+                                "Humidity:       "+mainHumidity+"\u0025\n\n"+
+                                "Visibility:        "+visibility+"Km\n\n"+
+                                "Wind Speed:  "+windSpeed+"m/s\n\n"+
+//                                "Wind Degree: "+windDegree+"\n"+
+//                                "DT: "+dt+"\n"+
+//                                "Type: "+sysType+"\n"+
+//                                "Id: "+sysId+"\n"+
+//                                "Country: "+sysCountry+"\n"+
+//                                "Sunrise: "+sysSunrise+"\n"+
+//                                "Sunset: "+sysSunset+"\n"+
+//                                "TimeZone: "+extraTimeZone+"\n"+
+//                                "Id: "+extraId+"\n"+
+//                                "Name: "+extraName+"\n"+
+//                                "Cod: "+extraCod+"\n"+
                                 "");
 
 
                     }
                     else {
-                        toast = Toast.makeText(getApplicationContext(), "Could not found!", Toast.LENGTH_SHORT);
+                        Toast toast = Toast.makeText(getApplicationContext(), "Could not found!", Toast.LENGTH_SHORT);
                         toast.show();
                     }
 
@@ -347,8 +388,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onErrorResponse(VolleyError error) {
 
-                Toast toast = Toast.makeText(getApplicationContext(), "error!", Toast.LENGTH_SHORT);
-                toast.show();
+                Toast toast;
+//                Toast toast = Toast.makeText(getApplicationContext(), "error!", Toast.LENGTH_SHORT);
+//                toast.show();
 
                 try {
                     String responseBody = new String( error.networkResponse.data, "utf-8" );
